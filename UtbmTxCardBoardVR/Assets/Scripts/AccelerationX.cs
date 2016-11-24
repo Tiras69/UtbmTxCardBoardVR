55519a47dbc33abe5e95c794139cd0877ca2fb22
Nanme <real90@hotmail.fr>
2016-11-24 08:21:37 +0100

    Add script for gyroscope movement & test

---
+++ UtbmTxCardBoardVR/Assets/Scripts/AccelerationX.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AccelerationX : MonoBehaviour {
+
+    public Text XAcceleration;
+    public Text YAcceleration;
+    public Text ZAcceleration;
+
+    private Gyroscope gyroscope;
+
+	// Use this for initialization
+	void Start () {
+
+        this.gyroscope = Input.gyro;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        XAcceleration.text = gyroscope.userAcceleration.x.ToString();
+        YAcceleration.text = gyroscope.userAcceleration.y.ToString();
+        ZAcceleration.text = gyroscope.userAcceleration.z.ToString();
+
+        Debug.DrawLine(Vector3.zero, gyroscope.userAcceleration);
+
+    }
+}
---
+++ UtbmTxCardBoardVR/Assets/Scripts/MoveGyroscope.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveGyroscope : Action
+{
+    private Gyroscope gyroscope;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    new void Start()
+    {
+        Input.gyro.enabled = true;  
+        this.gyroscope = Input.gyro;
+        this.rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = gyroscope.userAcceleration;
+        direction.y = 0.0f;
+        
+        Debug.DrawLine(_object.transform.position, _object.transform.position + direction, Color.green);
+        if (direction.magnitude > 0.5)
+            rb.AddForce(direction, ForceMode.Acceleration);
+        else
+            rb.ResetInertiaTensor();
+    }
+}
